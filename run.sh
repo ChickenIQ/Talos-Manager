4385a615e4938cad64310c937e3ca0ca0dc669cb
ChickenIQ <emibenzar@gmail.com>
2024-07-24 00:44:54 +0300

    Update example

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@ docker run -it --rm --network host \
     -v ~/.talos:/host/.talos \
     -v ~/.kube:/host/.kube \
     -v ./config.yaml:/host/config.yaml \
-    -v ~/.ansible/.vault_key:/host/.vault_key:ro \
     ghcr.io/chickeniq/talos-manager:latest
